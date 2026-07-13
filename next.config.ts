1be5a822848f31e240bd2a4c7b36e03c269c0df7
aihrtest-create <aihrtest@gmail.com>
2026-07-13 21:26:40 +0300

    Fix github pages build action config conflicts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const nextConfig: NextConfig = {
   output: isGithubPages ? 'export' : 'standalone',
   basePath: isGithubPages ? '/hello-park-invite' : '',
-  assetPrefix: isGithubPages ? '/hello-park-invite/' : undefined,
   images: {
     unoptimized: isGithubPages, // Required for next export
   },
